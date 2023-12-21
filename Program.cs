41d870e151faf49eb24406384c296b9d5cfd2172
Simon Isupov <is_se@mail.ru>
2023-12-21 20:13:55 +0300

    Init

--- Program.cs
+++ Program.cs
@@ -7,36 +7,15 @@ int ReadInt(string msg)
      return Convert.ToInt32(Console.ReadLine());
 } 
 
-int[] Generate(int size, int leftRange, int rightRange)
-{
-     int[] array = new int[];
-     for (int i = 0; i < array.Lenght; i++)
-     {
-        array[i] = new Random().Next(leftRange, rightRange+1);
-        Random rand = new Random();
-    }
-     return array;
-} 
-
-void PrintArray (int[] arrayForPrint)
-{
-    for (int i = 0; i < arrayForPrint.Length; i++)
-    {
-        System.Console.WriteLine(arrayForPrint[i]+" ");
-    }
-}
-int Osnovnoe (int[] inputArray)
-{
-    int count = 0;
-    for (int i = 0; i < inputArray.Length; i++)
-    {
-        if inputArray[i]%2=0;
-        count++;
-        retirn count;
-    }
-}
-
 int n = ReadInt("Введите число");
-int[] myArray = GenerateArray (n,100,999);
-PrintArray(myArray);
-System.Console.WriteLine(Osnovnoe(myArray));
+
+
+int sum;
+while (n>10)
+{
+    sum = sum + n%10;
+    n = n/10;
+}
+string q;
+if n == q && sum%2 = 0 break;
+else ReadInt();
